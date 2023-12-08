1126f05b9078dfcb7e748fe673bb73b39050d831
Ben-Long50 <benjlong50@gmail.com>
2023-12-07 23:04:37 -0800

    added a  tie game check and functionality that allows you to enter names for each player

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const game = (function() {
     const gameBoard = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
 
     const player1Token = 'X';
     const player2Token = 'O';
 
     function player1Move(boardRow, boardColumn) {
-        if(gameBoard[boardRow][boardColumn] === ' '){
+        if(gameBoard[boardRow][boardColumn] === ''){
             gameBoard[boardRow][boardColumn] = player1Token;
             domEvents.updateGameBoard();
             gameEndCheck();
@@ -18,7 +18,7 @@ const game = (function() {
     }
 
     function player2Move(boardRow, boardColumn) {
-        if(gameBoard[boardRow][boardColumn] === ' '){
+        if(gameBoard[boardRow][boardColumn] === ''){
             gameBoard[boardRow][boardColumn] = player2Token;
             domEvents.updateGameBoard();
             gameEndCheck();
@@ -42,23 +42,32 @@ const game = (function() {
         let endMessage = null;
         winPatterns.forEach(combination => {
             if(combination === 'XXX'){
-                endMessage =  'Player 1 Wins!'
-                resetGameBoard();
-                domEvents.updateGameBoard();
+                endMessage =  'Player 1 Wins!';
             }
             else if(combination === 'OOO'){
-                endMessage =  'Player 2 Wins!'
-                resetGameBoard();
-                domEvents.updateGameBoard();
+                endMessage =  'Player 2 Wins!';
             }
         });
+
+        const gameBoardFlat = gameBoard.flat();
+        if(!endMessage && !gameBoardFlat.includes('')){
+            endMessage = 'It\'s a Tie!';
+        }
+
+        if(endMessage) {
+            domEvents.showGameEndDialog(endMessage);
+            resetGameBoard();
+            domEvents.updateGameBoard();
+            gameFlow.turn = 1;
+        }
+
         console.log(endMessage);
     }
 
     function resetGameBoard() {
         for(let i = 0; i < gameBoard.length; i++){
             for(let j = 0; j < gameBoard[i].length; j++){
-                gameBoard[i][j] = ' ';
+                gameBoard[i][j] = '';
             }
         }
     }
@@ -67,6 +76,7 @@ const game = (function() {
         gameBoard: gameBoard,
         player1Move: player1Move,
         player2Move: player2Move,
+        resetGameBoard: resetGameBoard
     };
     
 })();
@@ -98,7 +108,18 @@ const domEvents = (function() {
     //cache DOM
     const gameBoard = document.querySelector('#game-board');
     const gameSpaces = gameBoard.querySelectorAll('.game-space');
-
+    const playerForm = document.querySelector('dialog#player-input-dialog');
+    const submitButton = playerForm.querySelector('button');
+    const player1 = document.querySelector('#player-1');
+    const player2 = document.querySelector('#player-2');
+    const gameEndDialog = document.querySelector('dialog#game-end-dialog');
+    const endMessage = document.querySelector('#end-message');
+    const closeButton = document.querySelector('#close-button');
+    const resetPlayersButton = document.querySelector('#reset-players-button');
+    const resetGameButton = document.querySelector('#reset-game-button');
+    const player1Input = document.querySelector('#player-1-name');
+    const player2Input = document.querySelector('#player-2-name');
+    
     //Event Listeners
     gameSpaces.forEach(gameSpace => {
         gameSpace.addEventListener('click', () => {
@@ -111,6 +132,28 @@ const domEvents = (function() {
         })
     })
 
+    submitButton.addEventListener('click', updatePlayerNames)
+
+    resetPlayersButton.addEventListener('click', showPlayerForm)
+
+    resetGameButton.addEventListener('click', () => {
+        game.resetGameBoard();
+        updateGameBoard();
+        gameFlow.turn = 1;
+    })
+
+    closeButton.addEventListener('click', () => {
+        player1Input.value = '';
+        player2Input.value = '';
+        gameEndDialog.close();
+    })
+
+    //DOM Functions
+    function updatePlayerNames() {
+        player1.textContent = player1Input.value;
+        player2.textContent = player2Input.value;
+    }
+
     function updateGameBoard() {
         const gameBoardFlat = game.gameBoard.flat();
         let index = 0;
@@ -120,8 +163,22 @@ const domEvents = (function() {
         })
     }
 
+    function showPlayerForm() {
+        playerForm.showModal();
+    }
+
+    function showGameEndDialog(message) {
+        endMessage.textContent = message;
+        gameEndDialog.showModal();
+
+    }
+
     return {
-        updateGameBoard: updateGameBoard
+        updateGameBoard: updateGameBoard,
+        showPlayerForm: showPlayerForm,
+        showGameEndDialog: showGameEndDialog
     }
 
 })();
+
+domEvents.showPlayerForm();
